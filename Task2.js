6494adab2b240f2cd0fa36415d329fd8ef3cc7a7
Mchab516 <59071181+Mchab516@users.noreply.github.com>
2023-01-24 19:21:00 +0100

    Add files via upload

---
+++ Task2.js
@@ -0,0 +1,13 @@
+let product_V1 = [[7, 9, 1],
+product_V2   [6, 0, 2]
+
+]
+
+function dotProduct(product_V1,product_V2){
+    let result = 0;
+    for(let i = 0; i < dotProduct.length; i++){
+        p = product_V1[0] * product_V2[0] + product_V1[1] * product_V2[1] + product_V1[2] * product_V2[2]
+      }
+    return p
+  }
+  
